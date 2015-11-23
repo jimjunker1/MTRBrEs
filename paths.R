ebc2849c892b4ce550280e5a41b70a77e851863c
Diego Barneche <diego.barneche@ymail.com>
2015-11-23 15:47:01 +1100

    file that creates output folders

---
+++ paths.R
@@ -0,0 +1,3 @@
+dir.create('output/data',  recursive=TRUE, showWarnings=FALSE)
+dir.create('output/figures',  recursive=TRUE, showWarnings=FALSE)
+dir.create('output/RDatafiles', recursive=TRUE, showWarnings=FALSE)
