3ce218679b722fc03ccf810efa00b6d1e7dddbc0
Diego Barneche <diego.barneche@ymail.com>
2015-11-23 15:49:51 +1100

    fixed relation between dataManipulation.R and figure.R

--- figures.R
+++ figures.R
@@ -1,6 +1,9 @@
 library(extrafont)
 library(fontcm)
 loadfonts()
+
+rm(list=ls())
+source('dataManipulation.R')
 source('R/functions-figures.R')
 
 to.pdf(plotResp(), 'output/figures/metRates.pdf', width=12, height=5.5)
