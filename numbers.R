81c510fa50575bef917abd98081febcb07bcf930
Diego Barneche <diego.barneche@ymail.com>
2016-01-30 13:46:25 +1100

    new numbers for paper

--- numbers.R
+++ numbers.R
@@ -1,14 +1,17 @@
+library(plyr)
 library(lme4)
 
 rm(list=ls())
 source('paths.R')
+source('R/functions-analyses.R')
 load('output/RDatafiles/analyses.RData')
 
-# compare q10's between both models
-r10  <-  exp(fixef(modelLmer)['(Intercept)'] + fixef(modelLmer)['lnMass']*1)
-r25  <-  exp((fixef(modelLmer)['(Intercept)'] + fixef(modelLmer)['Temp25']) + fixef(modelLmer)['lnMass']*1)
-q10  <-  (r25 / r10) ^ (10/(25-10))
+# model selection result
+deltaDF   <-  anova(modelLmer1, modelLmer2)['Chi Df'][2,]
+deltaDIC  <-  tfit2$BUGSoutput$DIC - tfit$BUGSoutput$DIC
 
-r10b  <-  exp(fixef(boltzLmer)['(Intercept)'] + fixef(boltzLmer)['lnMass']*1 + (fixef(boltzLmer)['invKT'] / 8.62e-5 * (1 / 293.15 - 1 / 283.15)))
-r25b  <-  exp(fixef(boltzLmer)['(Intercept)'] + fixef(boltzLmer)['lnMass']*1 + (fixef(boltzLmer)['invKT'] / 8.62e-5 * (1 / 293.15 - 1 / 298.15)))
-q10b  <-  (r25b / r10b) ^ (10/(25-10))
+# compare deltas between temperatures for each species
+species     <-  unique(metRates$Species)
+tempDeltas  <-  ldply(species, getTempDeltas)
+
+save.image('output/RDatafiles/numbers.RData')
