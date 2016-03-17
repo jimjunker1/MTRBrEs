bad58540772b4d14fc2ebca2293bd35a1db4a6ad
Diego Barneche <diego.barneche@ymail.com>
2016-03-17 12:29:19 +1100

    added cover letter

--- paper.R
+++ paper.R
@@ -10,3 +10,10 @@ rm(list=ls())
 knit('text/MS.Rmd', output=file.path(getwd(), 'text/MS.md'), quiet=TRUE, encoding = 'utf-8')
 # to word
 system('pandoc -o text/MS.docx text/MS.md -s -S --bibliography library.bib --csl functionalEcology.csl')
+
+##############
+# COVER LETTER 
+##############
+knit('text/coverLetter.Rmd', output=file.path(getwd(), 'text/coverLetter.md'), quiet=TRUE, encoding = 'utf-8')
+# to word
+system('pandoc -o text/coverLetter.docx text/coverLetter.md')
