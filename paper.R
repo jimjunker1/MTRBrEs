8e5c364d3f8cb19a04ef3617e90659f147fea727
Diego Barneche <diego.barneche@ymail.com>
2016-08-09 13:40:29 +1000

    first attempt to reply to reviewers comments

--- paper.R
+++ paper.R
@@ -17,3 +17,10 @@ system('pandoc -o text/MS.docx text/MS.md -s -S --bibliography library.bib --csl
 knit('text/coverLetter.Rmd', output=file.path(getwd(), 'text/coverLetter.md'), quiet=TRUE, encoding = 'utf-8')
 # to word
 system('pandoc -o text/coverLetter.docx text/coverLetter.md')
+
+####################
+# ANSWER TO REFEREES 
+####################
+knit('text/answers_to_reviewers.Rmd', output=file.path(getwd(), 'text/answers_to_reviewers.md'), quiet=TRUE, encoding = 'utf-8')
+# to word
+system('pandoc -o text/answers_to_reviewers.docx text/answers_to_reviewers.md -s -S --bibliography library.bib --csl ecology.csl')
